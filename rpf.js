5d40f034d63b05fb241a06d62a2b8e97b7e5ab34
Te Vallee <astronomemusic@gmail.com>
2014-10-02 21:43:55 -1000

    fixed indentation for readability

--- rpf.js
+++ rpf.js
@@ -2,12 +2,18 @@ var userChoice = prompt("rock, paper, or fireball?")
 
 var computerChoice = Math.random();
 console.log(computerChoice);
-if (computerChoice <=0.33) {computerChoice = ("rock");}
 
-else if (computerChoice > 0.34 && computerChoice < 0.67)
-	{computerChoice= ("paper");}
+if (computerChoice <=0.33){
+  computerChoice = ("rock");
+}
 
-else {computerChoice = ("fireball");}
+else if (computerChoice > 0.34 && computerChoice < 0.67){
+  computerChoice= ("paper");
+}
+
+else{
+  computerChoice = ("fireball");
+}
 
 document.getElementById("userChoice").innerHTML = "You chose " + userChoice;
 document.getElementById("computerChoice").innerHTML = "Computer chose " + computerChoice;
@@ -20,43 +26,44 @@ document.getElementById("results").innerHTML = result;
 //Paper covers rock.
 
 
-function compare(choice1, choice2){if(choice1 === choice2) {return "The result is a tie!";}
+function compare(choice1, choice2){
+  if(choice1 === choice2){
+    return "The result is a tie!";
+  }
 
-else if (choice1 === "rock") 
-
-{if(choice2 === "fireball"){return "rock blocks fireball";}
-
-
-else {return "paper covers rock";}}
-
-
-else if (choice1 === "paper"){if(choice2 === "rock") 
-
-	{return "paper covers rock";} else 
-
-{return "fireball engulfs paper";}}
+  else if (choice1 === "rock"){
+    if(choice2 === "fireball"){
+      return "rock blocks fireball";
+    }
 
 
-
-else if (choice1 === "fireball"){if(choice2 === "paper") 
-
-	{return "fireball engulfs paper";}
-
-
-else {return "rock blocks fireball";}
-
-
+  else{
+    return "paper covers rock";
+  }
 }
+
+
+  else if (choice1 === "paper"){
+    if(choice2 === "rock"){
+      return "paper covers rock";
+    } 
+  else {
+    return "fireball engulfs paper";
+  }
 }
 
 
 
+  else if (choice1 === "fireball"){
+    if(choice2 === "paper") {
+    return "fireball engulfs paper";
+    }
 
 
+  else {
+    return "rock blocks fireball";
+  }
 
 
-
-
-
-
-
+}
+}
